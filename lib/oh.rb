71b5e88c76598a13bd7f720799e85f3e337df792
Andrew Smith <andy@tinnedfruit.org>
2010-12-02 19:59:54 -0800

    sow oh

---
+++ lib/oh.rb
@@ -0,0 +1,3 @@
+class Oh
+  VERSION = '1.0.0'
+end
---
+++ test/test_oh.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "oh"
+
+class TestOh < Test::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
